c38a9526c46d870be8db26ed088c98c1a1f6b22a
Alex Davis, PhD <106837639+davisalexanderc@users.noreply.github.com>
2025-07-24 14:36:15 -0400

    fix(dqn): Add explicit tensor deletion in learn() to resolve memory leak

--- common/wrappers.py
+++ common/wrappers.py
@@ -191,7 +191,6 @@ class RewardWrapper(gym.Wrapper):
 
         obs, original_reward, terminated, truncated, info = self.env.step(action)
         # Store original score for logging purposes
-        info = info.copy() # Prevents a memory leak by copying the info dict
         info['original_reward'] = original_reward
 
         # Check if a pellet or power pellet was eaten
@@ -257,8 +256,6 @@ class RewardWrapper(gym.Wrapper):
 
         # Reset the environment
         obs, info = self.env.reset(**kwargs)
-
-        info = info.copy() # Prevents a memory leak by copying the info dict
 
         # Reset current lives
         self.current_lives = info.get('lives', self.current_lives)
